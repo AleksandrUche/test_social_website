e702d96bc4d78a341cdfba853eacea9b8e3e6f0a
AleksandrUche <zak.reizer@mail.ru>
2023-06-05 22:07:06 +0300

    updated

--- account/urls.py
+++ account/urls.py
@@ -1,17 +1,31 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
-
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     # url-адрес входа
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    # url-адрес выхода
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # url-адреса смены пароля
-    path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
-    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # # url-адрес выхода
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # # url-адреса смены пароля
+    # path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    # path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    # # url-адреса сброса пароля
+    # path('password-reset/',
+    #      auth_views.PasswordResetView.as_view(),
+    #      name='password_reset'),
+    # path('password-reset/done/',
+    #      auth_views.PasswordResetDoneView.as_view(),
+    #      name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/',
+    #      auth_views.PasswordResetConfirmView.as_view(),
+    #      name='password_reset_confirm'),
+    # path('password-reset/complete/',
+    #      auth_views.PasswordResetCompleteView.as_view(),
+    #      name='password_reset_complete'),
     # Панель
+    path("", include('django.contrib.auth.urls')),
     path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
 ]
