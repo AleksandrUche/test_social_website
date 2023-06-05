9ac916f429f6bdb22c8e342afa7cb134a3838074
AleksandrUche <zak.reizer@mail.ru>
2023-06-05 16:14:55 +0300

    added login, exit urls, password change

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
+    # url-адрес входа
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    # url-адрес выхода
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # url-адреса смены пароля
+    path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    # Панель
+    path("", views.dashboard, name="dashboard"),
 ]
